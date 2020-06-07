0ad53310d867b796659b6cbf01b675d702ed8d79
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2020-06-07 10:44:46 -0300

    Mobile app complete

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,22 +1,167 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Constants from 'expo-constants'
 import { Feather as Icon } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
-import { View, StyleSheet } from 'react-native'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { View, StyleSheet, Text, ScrollView, Image, Alert } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import MapView, { Marker } from 'react-native-maps'
+import { SvgUri } from 'react-native-svg'
+import * as Location from 'expo-location'
+import api from '../../services/api'
+
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+interface Point {
+  id: number;
+  name: string;
+  image: string;
+  latitude: number;
+  longitude: number;
+}
+
+interface Params {
+  uf: string;
+  city: string;
+}
 
 const Points = () => {
+  const [items, setItems] = useState<Item[]>([])
+  const [points, setPoints] = useState<Point[]>([])
+  const [selectedItems, setSelectedItems] = useState<number[]>([])
+  const [initialPosition, setInitialPositon] = useState<[number, number]>([0, 0])
+
+  const route = useRoute();
   const navigation = useNavigation();
+
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    api.get('items').then(response => {
+      setItems(response.data)
+    })
+  }, [])
+  
+  useEffect(() => {
+    const loadPosition = async () => {
+      const { status } = await Location.requestPermissionsAsync();
+
+      if (status !== 'granted') {
+        Alert.alert('Oooops...', 'Precisamos de sua permissao para obter a localizacao');
+        return
+      }
+
+      const location =  await Location.getCurrentPositionAsync();
+
+      const { latitude, longitude } = location.coords;
+
+      setInitialPositon([
+        latitude,
+        longitude
+      ])
+    }
+
+    loadPosition()
+  }, [])
+
+  useEffect(() => {
+    api.get('points', {
+      params: {
+        city: routeParams.city,
+        uf: routeParams.uf,
+        items: selectedItems
+      }
+    }).then(response => {
+      setPoints(response.data)
+    })
+  }, [selectedItems])
   
   const handleNavigateBack = () => {
     navigation.goBack()
   }
+
+  const handleNavidateToDetail = (id: number) => {
+    navigation.navigate('Detail', { point_id: id });
+  }
+
+  const handleSelectedItem = (id: number) => {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+
+      setSelectedItems(filteredItems)
+    } else {
+      setSelectedItems([ ...selectedItems, id ]);
+    }
+  }
+
   return (
+    <>
     <View style={styles.container}>
       <TouchableOpacity onPress={handleNavigateBack}>
         <Icon name="arrow-left" size={20} color="#34cb79" />
       </TouchableOpacity>
+
+      <Text style={styles.title}>Bem vindo</Text>
+      <Text>Encontre no mapa um ponto de coleta</Text>
+
+      <View style={styles.mapContainer}>
+        { initialPosition[0] !== 0 && (
+          <MapView 
+          style={styles.map} 
+          loadingEnabled={initialPosition[0] === 0}
+          initialRegion={{
+            latitude: initialPosition[0],
+            longitude: initialPosition[1],
+            latitudeDelta: 0.014,
+            longitudeDelta: 0.014
+          }} 
+        >
+          {points.map(point => (
+              <Marker
+              key={String(point.id)}
+              style={styles.mapMarker} 
+              onPress={() => handleNavidateToDetail(point.id)}
+              coordinate={{
+              latitude: point.latitude,
+              longitude: point.longitude
+            }}>
+              <View style={styles.mapMarkerContainer}>
+                <Image style={styles.mapMarkerImage} source={{ uri: point.image}} />
+                <Text style={styles.mapMarkerTitle}>{point.name}</Text>
+              </View>
+            </Marker>
+          ))}
+        </MapView>
+        ) }
+      </View>
     </View>
+    <View style={styles.itemsContainer}>
+      <ScrollView 
+        horizontal 
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 20 }}
+      >
+        {items.map(item => (
+          <TouchableOpacity 
+            activeOpacity={0.6} 
+            key={String(item.id)} 
+            style={[
+              styles.item,
+              selectedItems.includes(item.id) ? styles.selectedItem : {}]} 
+            onPress={() => handleSelectedItem(item.id)}
+          >
+            <SvgUri width={42} height={42} uri={item.image_url}/>
+            <Text style={styles.itemTitle}>{item.title}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+    </>
   )
 }
 
